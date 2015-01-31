807c5bc9c331074b7ed2acd6de78c6e0c6dbbebf
TheCjw <thecjw@qq.com>
2015-01-31 12:56:32 +0800

    First commit.

---
+++ gen_idx.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Author: TheCjw<thecjw@qq.com>
+# Created on 下午12:55 15/1/31
+
+__author__ = "TheCjw"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
